4b86239d78f893820f333556a70dd6d1338e1402
20khanh03 <20hoangkhanh03@gmail.com>
2024-11-23 17:20:36 +0700

    Add files via upload

---
+++ BLL/KhoThuocBLL.cs
@@ -0,0 +1,47 @@
+﻿using DAL;
+using DTO;
+
+namespace BLL
+{
+    public class KhoThuocBLL
+    {
+        private readonly KhoThuocDAL khoThuocDAL;
+
+        public KhoThuocBLL()
+        {
+            khoThuocDAL = new KhoThuocDAL();
+        }
+
+        public List<KhoThuocDTO> GetAll()
+        {
+            return khoThuocDAL.GetAll();
+        }
+
+        public KhoThuocDTO GetByMaThuoc(long maThuoc)
+        {
+            return khoThuocDAL.GetByMaThuoc(maThuoc);
+        }
+
+        public bool AddOrUpdateThuoc(KhoThuocDTO thuoc)
+        {
+            // Kiểm tra nếu thuốc đã tồn tại
+            var existingMedicine = khoThuocDAL.GetByMaThuoc(thuoc.MaThuoc);
+            if (existingMedicine != null)
+            {
+                // Thuốc đã tồn tại, tiến hành cập nhật
+                return khoThuocDAL.UpdateThuoc(thuoc);
+            }
+            else
+            {
+                // Thuốc chưa tồn tại, tiến hành thêm mới
+                return khoThuocDAL.AddThuoc(thuoc);
+            }
+        }
+
+        public List<KhoThuocDTO> SearchMedicines(string keyword)
+        {
+            return khoThuocDAL.Search(keyword);
+        }
+    }
+}
+
