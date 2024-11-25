18da2a2c29c5b0a94284d5e89f50e329d342cde0
20khanh03 <20hoangkhanh03@gmail.com>
2024-11-25 15:22:57 +0700

    Update KhoThuocBLL.cs

--- BLL/KhoThuocBLL.cs
+++ BLL/KhoThuocBLL.cs
@@ -17,31 +17,23 @@ namespace BLL
             return khoThuocDAL.GetAll();
         }
 
-        public KhoThuocDTO GetByMaThuoc(long maThuoc)
-        {
-            return khoThuocDAL.GetByMaThuoc(maThuoc);
-        }
-
         public bool AddOrUpdateThuoc(KhoThuocDTO thuoc)
         {
             // Kiểm tra nếu thuốc đã tồn tại
             var existingMedicine = khoThuocDAL.GetByMaThuoc(thuoc.MaThuoc);
             if (existingMedicine != null)
             {
-                // Thuốc đã tồn tại, tiến hành cập nhật
                 return khoThuocDAL.UpdateThuoc(thuoc);
             }
             else
             {
-                // Thuốc chưa tồn tại, tiến hành thêm mới
                 return khoThuocDAL.AddThuoc(thuoc);
             }
         }
 
         public List<KhoThuocDTO> SearchMedicines(string keyword)
         {
-            return khoThuocDAL.Search(keyword);
+            return khoThuocDAL.SearchKhoThuoc(keyword);
         }
     }
 }
-
